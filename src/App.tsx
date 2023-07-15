8b676f9806e4cff16b3aea036c34d2f84caebf0d
Islam <ueaten04@gmail.com>
2023-07-15 12:35:36 +0600

    fixed user bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import { Box } from "@mui/material";
 import Router from "./Router";
 import SideBar from "./components/SideBar/SideBar";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/config";
+import { useUserStore } from "./store/userStore";
+import { User as FirebaseUser } from "firebase/auth";
 
 function App() {
+	const { setUser, user } = useUserStore((state) => state);
+
+	useEffect(() => {
+		onAuthStateChanged(auth, (_user: FirebaseUser | null) => {
+			if (!auth.currentUser && !user) {
+				setUser(_user);
+			} else {
+				setUser(auth.currentUser);
+			}
+		});
+	}, [!auth.currentUser]);
+
 	return (
 		<Box display='flex'>
 			<SideBar />
--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -7,6 +7,7 @@ interface IUserState {
     user: FirebaseUser | null;
     login: (value: IUserData) => void;
     register: (value: IUserData) => void;
+    setUser: (value: FirebaseUser | null) => void
     logout: () => void;
 }
 
@@ -17,6 +18,7 @@ export const useUserStore = create<IUserState>((set) => ({
         const userCredential = await signInWithEmailAndPassword(auth, userData.email, userData.password);
         set({ user: userCredential.user });
     },
+    setUser: (user: FirebaseUser | null) => set({ user }),
     register: async (userData: IUserData) => {
         const userCredential = await createUserWithEmailAndPassword(auth, userData.email, userData.password);
         await updateProfile(userCredential.user, { displayName: userData.name });
