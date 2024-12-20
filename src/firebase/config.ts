1709b330c79a2a4a87aaaf5f8db2778eadc4c325
IslamTashiev <ueaten04@gmail.com>
2024-12-20 10:38:39 +0600

    add .env

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyBEyVLdN-3bHgrWvennbIw5OudLDZ-0sWo",
-	authDomain: "print-speed.firebaseapp.com",
-	projectId: "print-speed",
-	storageBucket: "print-speed.appspot.com",
-	messagingSenderId: "268722546191",
-	appId: "1:268722546191:web:db5dee3365f16c4760a57a",
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
 };
 const app = initializeApp(firebaseConfig);
 
